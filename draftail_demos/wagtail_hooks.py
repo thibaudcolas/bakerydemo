5199075b5e1129ffaf7f6d8de3461cf8b9025b57
Thibaud Colas <thibaudcolas@gmail.com>
2018-06-15 18:41:03 +0300

    [WIP] blockquote

--- draftail_demos/wagtail_hooks.py
+++ draftail_demos/wagtail_hooks.py
@@ -35,18 +35,11 @@ def register_strikethrough_feature(features):
             'label': 'S',
             # TODO This isn't possible right now due to a bug in Wagtail - needs fixing.
             # 'icon': 'M1024 512v64h-234.506c27.504 38.51 42.506 82.692 42.506 128 0 70.878-36.66 139.026-100.58 186.964-59.358 44.518-137.284 69.036-219.42 69.036-82.138 0-160.062-24.518-219.42-69.036-63.92-47.938-100.58-116.086-100.58-186.964h128c0 69.382 87.926 128 192 128s192-58.618 192-128c0-69.382-87.926-128-192-128h-512v-64h299.518c-2.338-1.654-4.656-3.324-6.938-5.036-63.92-47.94-100.58-116.086-100.58-186.964s36.66-139.024 100.58-186.964c59.358-44.518 137.282-69.036 219.42-69.036 82.136 0 160.062 24.518 219.42 69.036 63.92 47.94 100.58 116.086 100.58 186.964h-128c0-69.382-87.926-128-192-128s-192 58.618-192 128c0 69.382 87.926 128 192 128 78.978 0 154.054 22.678 212.482 64h299.518z',
+
             # This will be displayed in the toolbar tooltip - optional.
             'description': 'Strikethrough',
         })
     )
-
-
-@hooks.register('construct_whitelister_element_rules')
-def whitelister_strikethrough_feature():
-    return {
-        # Finally, we also need to configure Wagtail so it keeps the `s` tag when storing HTML in the DB.
-        's': allow_without_attributes,
-    }
 
 
 @hooks.register('register_rich_text_features')
@@ -56,10 +49,15 @@ def register_blockquote_feature(features):
     features.register_converter_rule(
         'contentstate', 'blockquote', {
             'from_database_format': {
-                'blockquote': BlockElementHandler('BLOCKQUOTE'),
+                'blockquote': BlockElementHandler('blockquote'),
             },
             'to_database_format': {
-                'block_map': {'BLOCKQUOTE': 'blockquote'}
+                'block_map': {'blockquote': {
+                    'element': 'blockquote',
+                    'props': {
+                        'class': 'u-blockquote',
+                    }
+                }}
             }
         })
 
@@ -68,17 +66,10 @@ def register_blockquote_feature(features):
         # The editor is `draftail`, and the feature is `blockquote`, and it's a Draftail "inline style" feature.
         'draftail', 'blockquote', draftail_features.BlockFeature({
             # This should be the same as the uppercase identifiers above.
-            'type': 'BLOCKQUOTE',
+            'type': 'blockquote',
             # This will be displayed in the toolbar.
             'label': '❝',
             'description': 'Blockquote',
             'element': 'blockquote',
         })
     )
-
-
-@hooks.register('construct_whitelister_element_rules')
-def whitelister_blockquote_feature():
-    return {
-        'blockquote': allow_without_attributes,
-    }

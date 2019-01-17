fd460ba6d7812e87801840f82385834a81dd0fa7
Thibaud Colas <thibaudcolas@gmail.com>
2019-01-18 00:35:07 +0200

    Tweak static files config

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')

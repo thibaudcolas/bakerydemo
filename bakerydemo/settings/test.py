0ab3f18835d97d059ad25ede09d89adde42c1a6f
Thibaud Colas <thibaudcolas@gmail.com>
2019-04-16 08:46:18 +0100

    Disable password validators to make it easier to create testing accounts

--- bakerydemo/settings/test.py
+++ bakerydemo/settings/test.py
@@ -2,3 +2,18 @@ from bakerydemo.settings.dev import *  # noqa
 
 # Make sure the site can be accessed from any host, e.g. for cross-browser testing.
 ALLOWED_HOSTS = "*"
+
+# Disable most validators to allow the user of simpler passwords.
+# We still want at least one validator to be configured to test password validation.
+AUTH_PASSWORD_VALIDATORS = [
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"}
+]

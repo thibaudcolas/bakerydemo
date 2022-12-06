cb6334960ba1b372999e29bd4295946e0853da0d
Thibaud Colas <thibaudcolas@gmail.com>
2022-12-06 23:42:50 +0000

    Switch back to Postgres

--- bakerydemo/settings/dev.py
+++ bakerydemo/settings/dev.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from .base import *  # noqa: F403, F401
 
 DEBUG = True
@@ -8,3 +10,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 WAGTAILADMIN_BASE_URL = "http://localhost:8000"
 
 ALLOWED_HOSTS = ["*"]
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)

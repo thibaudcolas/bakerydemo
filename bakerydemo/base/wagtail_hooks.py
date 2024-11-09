e595c60524581a5f4859c59dda61a8dd67761cf0
Thibaud Colas <thibaudcolas@gmail.com>
2024-11-09 16:15:09 +0000

    Footnotes UX improvements PoC

--- bakerydemo/base/wagtail_hooks.py
+++ bakerydemo/base/wagtail_hooks.py
@@ -92,3 +92,70 @@ class BakerySnippetViewSetGroup(SnippetViewSetGroup):
 # When using a SnippetViewSetGroup class to group several SnippetViewSet classes together,
 # you only need to register the SnippetViewSetGroup class with Wagtail:
 register_snippet(BakerySnippetViewSetGroup)
+
+
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+
+from draftjs_exporter.dom import DOM
+from wagtail import hooks
+from wagtail.admin.rich_text.converters.html_to_contentstate import (
+    InlineEntityElementHandler,
+)
+
+
+@hooks.register("register_rich_text_features")
+def register_footnotes_feature(features):
+    """
+    Registering the `footnotes` feature, which uses the `FOOTNOTES` Draft.js
+    entity type, and is stored as HTML with a
+    `<footnotes id="">short-id</footnotes>` tag.
+    """
+    feature_name = "footnotes"
+    type_ = "FOOTNOTES"
+
+    control = {"type": type_, "label": "Fn", "description": "Footnotes"}
+
+    features.register_editor_plugin(
+        "draftail",
+        feature_name,
+        draftail_features.EntityFeature(
+            control,
+            js=["wagtailadmin/js/draftail.js", "js/footnotes.js"],
+        ),
+    )
+
+    features.register_converter_rule(
+        "contentstate",
+        feature_name,
+        {
+            "from_database_format": {
+                "footnote[id]": FootnotesEntityElementHandler(type_)
+            },
+            "to_database_format": {
+                "entity_decorators": {type_: footnotes_entity_decorator}
+            },
+        },
+    )
+
+
+def footnotes_entity_decorator(props):
+    """
+    Draft.js ContentState to database HTML.
+    Converts the FOOTNOTES entities into a footnote tag.
+    """
+    return DOM.create_element("footnote", {"id": props["footnote"]}, props["children"])
+
+
+class FootnotesEntityElementHandler(InlineEntityElementHandler):
+    """
+    Database HTML to Draft.js ContentState.
+    Converts the footnote tag into a FOOTNOTES entity, with the right data.
+    """
+
+    mutability = "IMMUTABLE"
+
+    def get_attribute_data(self, attrs):
+        """
+        Take the ``footnote UUID`` value from the ``id`` HTML attribute.
+        """
+        return {"footnote": attrs["id"]}
---
+++ bakerydemo/static/js/footnotes.js
@@ -0,0 +1,180 @@
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
+  }
+
+  function setUUID(id) {
+    var $input = $("input#" + id);
+    var $displayValue = $("div#" + id + "_display-value");
+    if (!$input.val()) {
+      var uuid = uuidv4();
+      // Set hidden field
+      $input.val(uuid);
+      // Set displayed text
+      $displayValue.html(uuid.substring(0, 6));
+    }
+  }
+
+  const React = window.React;
+  const Modifier = window.DraftJS.Modifier;
+  const EditorState = window.DraftJS.EditorState;
+
+  class FootnoteSource extends React.Component {
+    constructor(props) {
+      super(props);
+      this.onClose = this.onClose.bind(this);
+    }
+
+    componentDidMount() {
+      const { editorState, entityType, onComplete } = this.props;
+
+      const content = editorState.getCurrentContent();
+      const selection = editorState.getSelection();
+
+      $(document.body).on("hidden.bs.modal", this.onClose);
+
+      $("body > .modal").remove();
+
+      $.ajax({
+        url: "/footnotes/footnotes_modal/",
+        success: function (data) {
+          $("body").append(data);
+          var $btn = $("#id_footnotes-ADD");
+          var $lst = $("#footnotes-listing");
+          var table = $("#footnotes-listing tbody");
+          table.empty();
+
+          // Add a "Create a new footnote" button
+          var $newbtn = $("<button class='button'>Create a new footnote</button>");
+          $lst.append($newbtn);
+          $newbtn.on("click", function () {
+            $btn.click();
+
+            $("#footnotes-modal").modal("hide");
+            document.querySelector(`#panel-child-content-footnotes-section`).scrollIntoView({ behavior: 'smooth' })
+            setTimeout(() => {
+              var uuid = document.querySelector("#id_footnotes-0-uuid_display-value").innerText;
+              document.querySelector("#id_footnotes-0-uuid_display-value").innerHTML = `<a href="#ref-${uuid}">${uuid.substring(0, 6)}</a>`;
+
+            // Uses the Draft.js API to create a new entity with the right data.
+            const contentWithEntity = content.createEntity(
+              entityType.type,
+              "IMMUTABLE",
+              {
+                footnote: uuid,
+              }
+            );
+            const entityKey = contentWithEntity.getLastCreatedEntityKey();
+
+            // We also add some text for the entity to be activated on.
+            const text = `[${uuid}]`;
+
+            const newContent = Modifier.replaceText(
+              content,
+              selection,
+              text,
+              null,
+              entityKey
+            );
+            const nextState = EditorState.push(
+              editorState,
+              newContent,
+              "insert-characters"
+            );
+
+            onComplete(nextState);
+            }, 300);
+          });
+
+          var live_footnotes = document.querySelectorAll(
+            "#id_footnotes-FORMS .w-panel"
+          );
+          Array.prototype.forEach.call(live_footnotes, function (value) {
+            const text = $(".public-DraftEditor-content", value).text();
+            const uuid = $('input[id*="-uuid"]', value)[0].value;
+            var row = $(
+              "<tr data-uuid=" +
+              uuid +
+              "><td>" +
+              text +
+              "</td><td>" +
+              uuid.substring(0, 6) +
+              "</td></tr>"
+            ).css({ cursor: "pointer" });
+            table.append(row);
+
+            row.on("click", function (event) {
+              const footnoteUUID = event.target.parentElement.dataset["uuid"];
+
+              // Uses the Draft.js API to create a new entity with the right data.
+              const contentWithEntity = content.createEntity(
+                entityType.type,
+                "IMMUTABLE",
+                {
+                  footnote: footnoteUUID,
+                }
+              );
+              const entityKey = contentWithEntity.getLastCreatedEntityKey();
+
+              // We also add some text for the entity to be activated on.
+              const shortKey = footnoteUUID.substring(0, 6);
+              const text = `[${shortKey}]`;
+
+              const newContent = Modifier.replaceText(
+                content,
+                selection,
+                text,
+                null,
+                entityKey
+              );
+              const nextState = EditorState.push(
+                editorState,
+                newContent,
+                "insert-characters"
+              );
+
+              onComplete(nextState);
+
+              $("#footnotes-modal").modal("hide");
+            });
+          });
+
+          $("#footnotes-modal").modal("show");
+        },
+        dataType: "html",
+      });
+    }
+
+    onClose(e) {
+      const { onClose } = this.props;
+      e.preventDefault();
+      onClose();
+    }
+
+    render() {
+      return null;
+    }
+  }
+
+  const Footnote = (props) => {
+    const { entityKey, contentState } = props;
+    const data = contentState.getEntity(entityKey).getData();
+    return React.createElement("sup", {
+      "id": `ref-${data.footnote}`,
+      "style": {
+        "color": "var(--w-color-secondary)",
+        "text-decoration": "underline",
+      },
+      onClick: () => document.querySelector(`#inline_child_footnotes-0-panel-content`).scrollIntoView({ behavior: 'smooth' }),
+    }, props.children);
+  };
+
+  window.draftail.registerPlugin({
+    type: "FOOTNOTES",
+    source: FootnoteSource,
+    decorator: Footnote,
+  });
